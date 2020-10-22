78fa27826a06491150522ed58ff9735c635d467b
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-10-21 20:13:46 -0500

    init commit

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((count) => count + 1);
+  };
+
+  const decrement = () => {
+    setCount((count) => count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <React.Fragment>
+      <h3>{count}</h3>
+      <button onClick={increment} className='btn'>
+        Add Count
+      </button>
+      <button onClick={decrement} className='btn'>
+        Delete Count
+      </button>
+      <button onClick={reset} className='btn'>
+        Reset Count
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default ErrorExample;
